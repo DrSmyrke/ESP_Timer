94f61c31e6e102f1ba7f6f771f83dd0abb439ab4
Jura Prokofev <Smyrke2005@yandex.ru>
2022-04-29 14:02:13 +0300

    Add files via upload

---
+++ src/timer.cpp
@@ -0,0 +1,59 @@
+#include "timer.h"
+// #include "driver/timer.h"
+
+//------------------- VARIABLES ------------------------
+portMUX_TYPE timerMux		= portMUX_INITIALIZER_UNLOCKED;
+uint8_t interruptFlag		= 0;
+uint16_t counter			= 0;
+uint16_t counterMax			= 0;
+//------------------------------------------------------
+void IRAM_ATTR onTimer()
+{
+	portENTER_CRITICAL_ISR( &timerMux );
+	if( counter++ >= counterMax ){
+		interruptFlag = 1;
+		counter = 0;
+	}
+	portEXIT_CRITICAL_ISR( &timerMux );
+}
+
+//------------------------------------------------------
+Timer::Timer(const uint8_t timerNum, const uint16_t samplingRate)
+{
+	/*
+	// инициализация таймера 0, группы 0
+	timer_config_t config;
+	config.divider = 80; // тактирование счетчика 1 мкс
+	config.counter_dir = TIMER_COUNT_UP; // прямой счет
+	config.counter_en = TIMER_START; // счетчик работает
+	config.alarm_en = TIMER_ALARM_DIS; // событие перезагрузка запрещено
+	config.auto_reload = TIMER_AUTORELOAD_DIS ; // аппаратная перезагрузка запрещена
+	timer_init(TIMER_GROUP_0, TIMER_0 , &config); // инициализация
+	*/
+
+	counterMax = samplingRate;
+	hw_timer_t* timer = timerBegin( timerNum, 80, true );	//Begin timer with 1 MHz frequency (80MHz/80)
+	timerAttachInterrupt(timer, &onTimer, true);			//Attach the interrupt to Timer1
+	unsigned int timerFactor = 1000000/samplingRate;		//Calculate the time interval between two readings, or more accurately, the number of cycles between two readings
+	timerAlarmWrite(timer, timerFactor, true);				//Initialize the timer
+	timerAlarmEnable(timer);
+}
+
+
+//------------------------------------------------------
+bool Timer::isInterrupt()
+{
+	return ( interruptFlag ) ? true : false;
+}
+
+//------------------------------------------------------
+void Timer::confirmInerrupt()
+{
+	interruptFlag = 0;
+}
+
+//------------------------------------------------------
+void Timer::counterReset()
+{
+	counter = 0;
+}
---
+++ src/timer.h
@@ -0,0 +1,18 @@
+#ifndef __MAIN_TIMER_H_
+#define __MAIN_TIMER_H_
+
+#include <stdint.h>
+#include "esp32-hal.h"
+
+
+class Timer
+{
+	public:
+		explicit Timer(const uint8_t timerNum, const uint16_t samplingRate);
+		bool isInterrupt();
+		void confirmInerrupt();
+		void counterReset();
+};
+
+
+#endif //__MAIN_TIMER_H_
